5ca24f08dc6745f7cb9dab9d3179be767cc937d5
Arman Harutyunyan <Arman Harutyunyan>
2016-04-11 17:07:56 +0400

    no message

--- core/src/com/frismos/unicorn/actor/Shadow.java
+++ core/src/com/frismos/unicorn/actor/Shadow.java
@@ -13,16 +13,17 @@ public class Shadow extends Image {
     public Shadow(GameStage gameStage, Enemy enemy) {
         super(gameStage.game.atlasManager.get("gfx/@atlas/pack.atlas", TextureAtlas.class).findRegion("shadow"));
         setSize(enemy.getWidth() * 1.6f, enemy.getWidth() / 2);
-        setColor(enemy.getColor().r, enemy.getColor().g, enemy.getColor().b, .7f);
+        setColor(enemy.getColor().r, enemy.getColor().g, enemy.getColor().b, 1f);
         gameStage.addActor(this);
         setZIndex(gameStage.background.getZIndex() + 1);
     }
-
-    @Override
+/* @Override
     public void draw(Batch batch, float parentAlpha) {
-        batch.setBlendFunction(GL20.GL_SRC_ALPHA, GL20.GL_ONE);
+        //GL20.GL_DST_COLOR, GL20.GL_DST_COLOR, GL20.GL_ONE_MINUS_SRC_ALPHA
+        batch.setBlendFunction(GL20.GL_ONE, GL20.GL_DST_COLOR);
         super.draw(batch, parentAlpha);
         batch.setBlendFunction(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 
-    }
+    }*/
+
 }
